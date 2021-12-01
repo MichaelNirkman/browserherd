6d445a19882b29b08aa56f2e9cec67036a1f949f
Mikko Nirkko <nirkmik@gmail.com>
2021-12-01 21:21:36 +0200

    fix start script

--- start.sh
+++ start.sh
@@ -7,11 +7,11 @@ venv_path=$current_dir/$venv_name
 if [[ -d "$venv_path" ]]
 then
     echo "Virtual env found, activating"
-    source ~/dev/browserherd/$venv_name/bin/activate
+    source $venv_path/bin/activate
 else
     echo "Virtual Env with name $venv_name not found, running pip install"
     python3 -m venv $venv_name
-    source ~/dev/browserherd/$venv_name/bin/activate
+    source $venv_path/bin/activate
     pip3 install -r requirements.txt
 fi
 
